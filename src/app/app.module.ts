c7c74e1e6f6f342f7b1d0037e2017b241948058b
Mauro-Pereira <mauropereira1096@gmail.com>
2022-03-17 00:41:43 -0300

    adicionando feed

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,6 +9,7 @@ import { MatIconModule } from '@angular/material/icon';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatInputModule } from '@angular/material/input';
 import { MatButtonModule } from '@angular/material/button';
+import { MatCardModule } from '@angular/material/card';
 import { LoginComponent } from './login/login.component';
 import { FeedComponent } from './feed/feed.component';
 
@@ -27,7 +28,8 @@ import { FeedComponent } from './feed/feed.component';
     MatIconModule,
     MatFormFieldModule,
     MatInputModule,
-    MatButtonModule
+    MatButtonModule,
+    MatCardModule
   ],
   providers: [],
   bootstrap: [AppComponent]
